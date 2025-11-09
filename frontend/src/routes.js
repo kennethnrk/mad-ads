c7366a225ea7a17d228da32b4d31b6a1c1c1b1e3
Kenneth <Kenneth012004@outlook.com>
2025-11-08 20:09:09 -0500

    Initial products page

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -29,7 +29,7 @@ const routes = [
     component: <ContentCreator />,
   },
   {
-    name: "Main Dashboard",
+    name: "Products",
     layout: "/admin",
     path: "default",
     icon: <MdHome className="h-6 w-6" />,
